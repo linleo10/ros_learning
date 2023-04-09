3edaeba181c9d39555b606dbaae48cee52a9baa6
root <root@dusktown.com>
2023-04-09 04:27:35 +0200

    cleaning

--- build/CMakeFiles/params_learning_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/params_learning_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/params_learning_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros_ws/src/params_learning
-# Build directory: /home/ros_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
